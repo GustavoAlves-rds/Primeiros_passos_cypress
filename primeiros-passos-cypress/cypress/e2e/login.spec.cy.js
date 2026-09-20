ab15410aa40b2d631618878971316ec2d37fc9a9
Gustavo Alves <gustavoalvesrodrigues007@gmail.com>
2026-09-20 14:49:59 -0300

    Added selector list

--- primeiros-passos-cypress/cypress/e2e/login.spec.cy.js
+++ primeiros-passos-cypress/cypress/e2e/login.spec.cy.js
@@ -1,19 +1,28 @@
+const selectorlist = {
+  usernameField: '[name="username"]',
+  passwordFild: '[name="password"]',
+  loginButton: '.oxd-button',
+  sectionTitle: '.oxd-topbar-header-breadcrumb',
+  wrongCredentialAlert: '.oxd-alert-content > .oxd-text'
+}
+
+
 describe('Orange HRM Tests', () => {
   it('Login com sucesso', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('ADMIN')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
+    cy.get(selectorlist.usernameField).type('ADMIN')
+    cy.get(selectorlist.passwordFild).type('admin123')
+    cy.get(selectorlist.loginButton).click()
     cy.location ('pathname').should ('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb').contains('Dashboard')
+    cy.get(selectorlist.sectionTitle).contains('Dashboard')
   })
 
    it('Login Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Teste')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-alert-content > .oxd-text')
+    cy.get(selectorlist.usernameField).type('Teste')
+    cy.get(selectorlist.passwordFild).type('admin123')
+    cy.get(selectorlist.loginButton).click()
+    cy.get(selectorlist.wrongCredentialAlert)
    
   })
 
